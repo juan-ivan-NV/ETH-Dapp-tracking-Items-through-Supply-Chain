54f1474bf8ff6f5dcea1f19ff02469502e29d80a
juan-ivan-NV <coldcram14@hotmail.com>
2022-11-10 23:40:49 -0600

    gper;y network added

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+const HDWallet = require('truffle-hdwallet-provider');
+const infuraKey = "******************";
+const mnemonic = "xxxxxxx metamask mnemonic xxxxxxxxxxxxx";
+
 module.exports = {
   networks: {
     development: {
@@ -5,5 +9,11 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraKey}`),
+      network_id: "*",
+      gas: 4500000,
+      gasPrice: 10000000000
+    },
   }
 };
